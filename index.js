5141c0dd651cf1a8f45ff53faf03c471c1bdd264
SAIBMUN <saibmun@inf.upv.es>
2021-11-03 11:21:15 +0100

    scketio

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./src/database/config');
+const { connections } = require('mongoose');
 require('dotenv').config();
 
 // Create express server/application
@@ -18,6 +19,37 @@ app.use( cors() );
 
 // Sent and receive json format
 app.use( express.json() );
+const serverHttp = require('http').Server(app);
+const io = require("socket.io")(serverHttp, {
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"]
+    }
+  });
+
+var conections = {};
+
+io.on('connection', function(socket){
+    socket.on('send-message', function(data){
+        socket.broadcast.emit('text-event', 'Hola ' + data.receiver + ' soy ' + data.sender);
+    })
+    socket.on('send-idConnection', function(data){
+        conections[data] = socket;
+        console.log(Object.keys(conections).length);
+        socket.emit('connection-event', Object.keys(conections).length);
+    })
+    socket.on('disconnect', () =>{;
+
+        for (var k in conections){
+             if(conections[k] == socket){
+                delete conections[k];
+            }
+        }
+        console.log(Object.keys(conections).length);
+    })
+})
+
+
 
 
 // Routes
@@ -29,8 +61,11 @@ app.use( '/api/post', require('./src/routers/post.router') );
 app.use( '/api/channel', require('./src/routers/channel.router') );
 app.use( '/api/user', require('./src/routers/user.router') );
 
+/*app.listen( process.env.PORT, () => {
+    console.log(`Server app in port ${ process.env.PORT }`);
+});*/
 
-app.listen( process.env.PORT, () => {
-    console.log(`Server in port ${ process.env.PORT }`);
+serverHttp.listen( process.env.PORT, () => {
+    console.log(`Server http in port ${ process.env.PORT }`);
 });
 
