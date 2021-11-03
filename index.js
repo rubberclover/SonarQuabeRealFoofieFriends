b6558113f099451b10a3f7a7b0e0e3d827393604
SAIBMUN <saibmun@inf.upv.es>
2021-11-03 11:25:21 +0100

    server io

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./src/database/config');
-const { connections } = require('mongoose');
 require('dotenv').config();
 
 // Create express server/application
@@ -27,25 +26,25 @@ const io = require("socket.io")(serverHttp, {
     }
   });
 
-var conections = {};
+var connections = {};
 
 io.on('connection', function(socket){
     socket.on('send-message', function(data){
         socket.broadcast.emit('text-event', 'Hola ' + data.receiver + ' soy ' + data.sender);
     })
     socket.on('send-idConnection', function(data){
-        conections[data] = socket;
-        console.log(Object.keys(conections).length);
-        socket.emit('connection-event', Object.keys(conections).length);
+        connections[data] = socket;
+        console.log(Object.keys(connections).length);
+        socket.emit('connection-event', Object.keys(connections).length);
     })
     socket.on('disconnect', () =>{;
 
-        for (var k in conections){
-             if(conections[k] == socket){
-                delete conections[k];
+        for (var k in connections){
+             if(connections[k] == socket){
+                delete connections[k];
             }
         }
-        console.log(Object.keys(conections).length);
+        console.log(Object.keys(connections).length);
     })
 })
 
